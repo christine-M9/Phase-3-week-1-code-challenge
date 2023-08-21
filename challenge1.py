49e661cd9ca85128e4a2e20517e506c79fd2527d
Christine <christinekhekama99@gmail.com>
2023-08-21 23:52:13 +0300

    time converter

--- challenge1.py
+++ challenge1.py
@@ -1,11 +1,18 @@
-def convert_to_24_hour(hour, minute, time):
-    if time == "pm" and hour != 12:
-        hour += 12
-    elif time == "am" and hour == 12:
-        hour = 0
-        
-    return f"{hour:02}{minute:02}"
+def time_converter():
+    if TIME_IN_12_HOUR_CLOCK_SYSTEM[-2:] == "am":
+        if TIME_IN_12_HOUR_CLOCK_SYSTEM[:2] == "12":
+            return "The time in the 24-hour clock system is 00:00hrs."
+        else:
+            return "The time in the 24-hour clock system is " + TIME_IN_12_HOUR_CLOCK_SYSTEM[:-2] + "hrs."
 
+    elif TIME_IN_12_HOUR_CLOCK_SYSTEM[-2:] == "pm":
+        if TIME_IN_12_HOUR_CLOCK_SYSTEM[:2] == "12":
+            return "The time in the 24-hour clock system is 12:00hrs."
+        else:
+            return "The time in the 24-hour clock system is " + str(12 + int(TIME_IN_12_HOUR_CLOCK_SYSTEM[:2])) + TIME_IN_12_HOUR_CLOCK_SYSTEM[2:-2] + "hrs."
 
-print(convert_to_24_hour(12, 30, "am"))  # (place the time to be converted e.g (12, 30, "am")
-print(convert_to_24_hour(12, 30, "pm"))  #(place the time to be converted e.g (12, 30, "pm")
+    else:
+        return "Try again."
+
+TIME_IN_12_HOUR_CLOCK_SYSTEM = input("What is your time?")
+print(time_converter())
